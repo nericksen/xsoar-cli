3229cc10100c93545eee8563e929f1002f2ac9b0
nericksen <nericksen@paloaltonetworks.com>
2021-06-09 20:06:11 -0400

    Remove commented code from load function in xsoar.py

--- xsoar.py
+++ xsoar.py
@@ -457,38 +457,8 @@ class XSOARShell(Cmd):
         Example:
           XSOAR:> load
         """
-        """
-        # Print all saved configs
-        saved_configs = os.listdir("saved")
-        if len(saved_configs) == 0:
-            return "No saved configs. Run save command first"
-        for index, config in enumerate(saved_configs):
-            print(f"{[index]} {config}")
-        selected_index = input("Which config should be loaded (input number from above)? ")
-        selected_config_file = saved_configs[int(selected_index)]
-        if not XSOAR_API_KEY:
-            return "No API key configured for xsoar instance"
-        with open(f"saved/{selected_config_file}", "r") as f:
-            body = json.loads(f.read())
-        instance_name = input("Enter the instance name to create: ")
-        body["name"] = instance_name
-        
-        #print(body)
-        #with open("body.json", "w+") as f:
-        #    f.write(json.dumps(body))
-        headers = {
-            "content-type": "application/json",
-            "accept": "application/json",
-            "Authorization": XSOAR_API_KEY
-        }
-        res = requests.put(f"{XSOAR_URL}/settings/integration", json=body, headers=headers, verify=False)
-
-        if res.ok:
-            print(f"Successfully uploaded {selected_config_file} to {XSOAR_URL}")
-        else:
-            print(f"Error uploading...{res.text}")
-        """
         load_config(args, XSOAR_API_KEY, XSOAR_URL)
+
     def do_run(self, args):
         """
         Run the specified command
